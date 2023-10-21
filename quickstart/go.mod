9c9d0a990315f3ffaf60e6856f7786bd7e3ae315
nazmulhaquenahin <nahin.cdr@gmail.com>
2023-10-21 10:42:46 +0600

    completed

--- quickstart/go.mod
+++ quickstart/go.mod
@@ -2,12 +2,13 @@ module async-quickstart
 
 go 1.21.1
 
+require github.com/hibiken/asynq v0.24.1
+
 require (
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/uuid v1.3.1 // indirect
-	github.com/hibiken/asynq v0.24.1 // indirect
 	github.com/redis/go-redis/v9 v9.2.1 // indirect
 	github.com/robfig/cron/v3 v3.0.1 // indirect
 	github.com/spf13/cast v1.5.1 // indirect
--- quickstart/workers/workers.go
+++ quickstart/workers/workers.go
@@ -1,23 +1,28 @@
-package workers
+package main
 
 import (
-	"context"
-	"encoding/json"
+	"async-quickstart/task"
 	"github.com/hibiken/asynq"
 	"log"
 )
 
+// workers.go
 func main() {
-	server := asynq.NewServer(
+	srv := asynq.NewServer(
 		asynq.RedisClientOpt{Addr: "localhost:6379"},
 		asynq.Config{Concurrency: 10},
 	)
+
 	mux := asynq.NewServeMux()
 	mux.HandleFunc(task.TypeWelcomeEmail, task.HandleWelcomeEmailTask)
 	mux.HandleFunc(task.TypeReminderEmail, task.HandleReminderEmailTask)
 
-	if err := server.Run(mux); err != nil {
+	if err := srv.Run(mux); err != nil {
 		log.Fatal(err)
 	}
+}
 
+type emailTaskPayload struct {
+	// ID for the email recipient.
+	UserID int
 }
